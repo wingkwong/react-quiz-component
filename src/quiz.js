d47de50ae02752eaddcf06fe9f8218ec84c3170f
wingkwong <wingkwong.code@gmail.com>
2018-05-11 20:50:12 +0800

    added temp quiz obj

---
+++ src/quiz.js
@@ -0,0 +1,23 @@
+export const quiz =  {
+  "quizTitle": "Quiz Title",
+  "questions": [
+    {
+      "question": "Q1",
+      "answers": [
+        "A1",
+        "A2",
+        "A3",
+        "A4"
+      ],
+      "correctAnswer": "1"
+    },
+    {
+      "question": "Q2",
+      "answers": [
+        "True",
+        "False"
+      ],
+      "correctAnswer": "1"
+    }
+  ]
+} 
