8b22b056a546affe86823f98509a27b9289ef069
wingkwong <wingkwong.code@gmail.com>
2018-05-13 15:32:08 +0800

    added questionType

--- src/quiz.js
+++ src/quiz.js
@@ -3,6 +3,7 @@ export const quiz =  {
   "questions": [
     {
       "question": "Q1",
+      "questionType": "text",
       "answers": [
         "A1",
         "A2",
@@ -13,9 +14,21 @@ export const quiz =  {
     },
     {
       "question": "Q2",
+      "questionType": "text",
       "answers": [
         "True",
         "False"
+      ],
+      "correctAnswer": "1"
+    },
+    {
+      "question": "Q3",
+      "questionType": "photo",
+      "answers": [
+        "https://dummyimage.com/600x400/000/fff&text=A",
+        "https://dummyimage.com/600x400/000/fff&text=B",
+        "https://dummyimage.com/600x400/000/fff&text=C",
+        "https://dummyimage.com/600x400/000/fff&text=D"
       ],
       "correctAnswer": "1"
     }
