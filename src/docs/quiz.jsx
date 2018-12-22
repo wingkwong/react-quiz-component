3ed6bbfc1fc649bdc06ad0f726f53a66dc350824
wingkwong <wingkwong.code@gmail.com>
2018-12-22 13:12:07 +0800

    added messageForCorrectAnswer, messageForIncorrectAnswer and explanation

--- src/docs/quiz.jsx
+++ src/docs/quiz.jsx
@@ -10,7 +10,10 @@ export const quiz =  {
         "this.state",
         "this.values"
       ],
-      "correctAnswer": "3"
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
       "question": "ReactJS is developed by _____?",
@@ -19,7 +22,10 @@ export const quiz =  {
         "Google Engineers",
         "Facebook Engineers"
       ],
-      "correctAnswer": "2"
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
       "question": "ReactJS is an MVC based framework?",
@@ -28,7 +34,10 @@ export const quiz =  {
         "True",
         "False"
       ],
-      "correctAnswer": "2"
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
       "question": "Which of the following concepts is/are key to ReactJS?",
@@ -38,7 +47,10 @@ export const quiz =  {
         "Event delegation model",
         "Both of the above",
       ],
-      "correctAnswer": "3"
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
       "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
@@ -49,7 +61,10 @@ export const quiz =  {
         "https://dummyimage.com/600x400/000/fff&text=C",
         "https://dummyimage.com/600x400/000/fff&text=D"
       ],
-      "correctAnswer": "1"
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Correct answer. Good job.",
+      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
+      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }
   ]
-}
+} 
