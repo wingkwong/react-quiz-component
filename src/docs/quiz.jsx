2bf4481e5b1c8b89ff8210d107dc5f1f083c8da0
աɨռɢӄաօռɢ <wingkwong.code@gmail.com>
2021-12-11 13:36:50 +0800

    v0.3.9 (#71)
    * add: segment to quiz config
    * split Core in smaller components
    * move Explanation to separate component
    * fix: import to make it available for debugging
    * fix: login in renderAnswerInResult
    * fix: render of Explanation
    * fix: add missing docs
    * Support for choosing number of questions to be used (#70)
    * Support for choosing nr of questions to be used
    * Custom nr of questions also when shuffle = false
    * Use all questions if custom nr is too big
    
    * 0.3.9
    
    Co-authored-by: Pavlo_Oliinyk1 <pashades@gmail.com>
    Co-authored-by: Erica Weistrand <34507514+Weistrand@users.noreply.github.com>

--- src/docs/quiz.jsx
+++ src/docs/quiz.jsx
@@ -1,107 +1,110 @@
 const segment = {
-  basic: "basic",
-  medium: "medium",
-  advanced: "advanced",
+  basic: 'basic',
+  medium: 'medium',
+  advanced: 'advanced',
 };
 
-export const quiz =  {
-  "quizTitle": "React Quiz Component Demo",
-  "quizSynopsis": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim",
-  "questions": [
+const quiz = {
+  quizTitle: 'React Quiz Component Demo',
+  quizSynopsis: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim',
+  nrOfQuestions: '4',
+  questions: [
     {
-      "question": "How can you access the state of a component from inside of a member function?",
-      "questionPic": "https://dummyimage.com/600x400/000/fff&text=X",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
-        "this.getState()",
-        "this.prototype.stateValue",
-        "this.state",
-        "this.values"
+      question: 'How can you access the state of a component from inside of a member function?',
+      questionPic: 'https://dummyimage.com/600x400/000/fff&text=X',
+      questionType: 'text',
+      answerSelectionType: 'single',
+      answers: [
+        'this.getState()',
+        'this.prototype.stateValue',
+        'this.state',
+        'this.values',
       ],
-      "correctAnswer": "3",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20",
-      "segment": segment.advanced
+      correctAnswer: '3',
+      messageForCorrectAnswer: 'Correct answer. Good job.',
+      messageForIncorrectAnswer: 'Incorrect answer. Please try again.',
+      explanation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      point: '20',
+      segment: segment.advanced,
     },
     {
-      "question": "ReactJS is developed by _____?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
-        "Google Engineers",
-        "Facebook Engineers"
+      question: 'ReactJS is developed by _____?',
+      questionType: 'text',
+      answerSelectionType: 'single',
+      answers: [
+        'Google Engineers',
+        'Facebook Engineers',
       ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20",
-      "segment": segment.basic
+      correctAnswer: '2',
+      messageForCorrectAnswer: 'Correct answer. Good job.',
+      messageForIncorrectAnswer: 'Incorrect answer. Please try again.',
+      explanation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      point: '20',
+      segment: segment.basic,
     },
     {
-      "question": "ReactJS is an MVC based framework?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
-        "True",
-        "False"
+      question: 'ReactJS is an MVC based framework?',
+      questionType: 'text',
+      answerSelectionType: 'single',
+      answers: [
+        'True',
+        'False',
       ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "10"
+      correctAnswer: '2',
+      messageForCorrectAnswer: 'Correct answer. Good job.',
+      messageForIncorrectAnswer: 'Incorrect answer. Please try again.',
+      explanation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      point: '10',
     },
     {
-      "question": "Which of the following concepts is/are key to ReactJS?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
-        "Component-oriented design",
-        "Event delegation model",
-        "Both of the above",
+      question: 'Which of the following concepts is/are key to ReactJS?',
+      questionType: 'text',
+      answerSelectionType: 'single',
+      answers: [
+        'Component-oriented design',
+        'Event delegation model',
+        'Both of the above',
       ],
-      "correctAnswer": "3",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "30",
-      "segment": segment.medium
+      correctAnswer: '3',
+      messageForCorrectAnswer: 'Correct answer. Good job.',
+      messageForIncorrectAnswer: 'Incorrect answer. Please try again.',
+      explanation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      point: '30',
+      segment: segment.medium,
     },
     {
-      "question": "Lorem ipsum dolor sit amet, consectetur adipiscing elit,",
-      "questionType": "photo",
-      "answerSelectionType": "single",
-      "answers": [
-        "https://dummyimage.com/600x400/000/fff&text=A",
-        "https://dummyimage.com/600x400/000/fff&text=B",
-        "https://dummyimage.com/600x400/000/fff&text=C",
-        "https://dummyimage.com/600x400/000/fff&text=D"
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+      questionType: 'photo',
+      answerSelectionType: 'single',
+      answers: [
+        'https://dummyimage.com/600x400/000/fff&text=A',
+        'https://dummyimage.com/600x400/000/fff&text=B',
+        'https://dummyimage.com/600x400/000/fff&text=C',
+        'https://dummyimage.com/600x400/000/fff&text=D',
       ],
-      "correctAnswer": "1",
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
+      correctAnswer: '1',
+      messageForCorrectAnswer: 'Correct answer. Good job.',
+      messageForIncorrectAnswer: 'Incorrect answer. Please try again.',
+      explanation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      point: '20',
     },
     {
-      "question": "What are the advantages of React JS?",
-      "questionType": "text",
-      "answerSelectionType": "multiple",
-      "answers": [
-        "React can be used on client and as well as server side too",
-        "Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps",
-        "React components have lifecycle events that fall into State/Property Updates",
-        "React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer"
+      question: 'What are the advantages of React JS?',
+      questionType: 'text',
+      answerSelectionType: 'multiple',
+      answers: [
+        'React can be used on client and as well as server side too',
+        'Using React increases readability and makes maintainability easier. Component, Data patterns improves readability and thus makes it easier for manitaining larger apps',
+        'React components have lifecycle events that fall into State/Property Updates',
+        'React can be used with any other framework (Backbone.js, Angular.js) as it is only a view layer',
       ],
-      "correctAnswer": [1, 2, 4],
-      "messageForCorrectAnswer": "Correct answer. Good job.",
-      "messageForIncorrectAnswer": "Incorrect answer. Please try again.",
-      "explanation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      "point": "20"
+      correctAnswer: [1, 2, 4],
+      messageForCorrectAnswer: 'Correct answer. Good job.',
+      messageForIncorrectAnswer: 'Incorrect answer. Please try again.',
+      explanation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      point: '20',
     },
-  ]
-}
+  ],
+};
+
+export default quiz;
--- src/lib/Core.jsx
+++ src/lib/Core.jsx
@@ -1,18 +1,20 @@
-import React, {useState, useEffect, useCallback, Fragment} from 'react';
-import PropTypes from 'prop-types';
-import QuizResultFilter from "./core-components/QuizResultFilter";
-import {checkAnswer, rawMarkup} from "./core-components/helpers";
-import InstantFeedback from "./core-components/InstantFeedback";
-import Explanation from "./core-components/Explanation";
+import React, {
+  useState, useEffect, useCallback, Fragment,
+} from 'react';
+import QuizResultFilter from './core-components/QuizResultFilter';
+import { checkAnswer, rawMarkup } from './core-components/helpers';
+import InstantFeedback from './core-components/InstantFeedback';
+import Explanation from './core-components/Explanation';
 
-const Core = ({questions, appLocale, showDefaultResult, onComplete, customResultPage, showInstantFeedback, continueTillCorrect}) => {
+const Core = function ({
+  questions, appLocale, showDefaultResult, onComplete, customResultPage, showInstantFeedback, continueTillCorrect,
+}) {
   const [incorrectAnswer, setIncorrectAnswer] = useState(false);
   const [correctAnswer, setCorrectAnswer] = useState(false);
   const [showNextQuestionButton, setShowNextQuestionButton] = useState(false);
   const [endQuiz, setEndQuiz] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [buttons, setButtons] = useState({});
-  const [buttonClasses, setButtonClasses] = useState({});
   const [correct, setCorrect] = useState([]);
   const [incorrect, setIncorrect] = useState([]);
   const [userInput, setUserInput] = useState([]);
@@ -35,7 +37,7 @@ const Core = ({questions, appLocale, showDefaultResult, onComplete, customResult
   }, [currentQuestionIndex]);
 
   useEffect(() => {
-    let {answerSelectionType} = question;
+    const { answerSelectionType } = question;
     // Default single to avoid code breaking due to automatic version upgrade
     setAnswerSelectionType(answerSelectionType || 'single');
   }, [question, currentQuestionIndex]);
@@ -44,16 +46,16 @@ const Core = ({questions, appLocale, showDefaultResult, onComplete, customResult
     if (endQuiz) {
       let totalPointsTemp = 0;
       let correctPointsTemp = 0;
-      for (let i = 0; i < questions.length; i++) {
+      for (let i = 0; i < questions.length; i += 1) {
         let point = questions[i].point || 0;
         if (typeof point === 'string' || point instanceof String) {
-          point = parseInt(point)
+          point = parseInt(point);
         }
 
-        totalPointsTemp = totalPointsTemp + point;
+        totalPointsTemp += point;
 
         if (correct.includes(i)) {
-          correctPointsTemp = correctPointsTemp + point
+          correctPointsTemp += point;
         }
       }
       setTotalPoints(totalPointsTemp);
@@ -66,16 +68,16 @@ const Core = ({questions, appLocale, showDefaultResult, onComplete, customResult
       numberOfQuestions: questions.length,
       numberOfCorrectAnswers: correct.length,
       numberOfIncorrectAnswers: incorrect.length,
-      questions: questions,
-      userInput: userInput,
-      totalPoints: totalPoints,
-      correctPoints: correctPoints
-    })
+      questions,
+      userInput,
+      totalPoints,
+      correctPoints,
+    });
   }, [totalPoints, correctPoints]);
 
   useEffect(() => {
     if (endQuiz && onComplete !== undefined && questionSummary !== undefined) {
-      onComplete(questionSummary)
+      onComplete(questionSummary);
     }
   }, [endQuiz, questionSummary]);
 
@@ -86,46 +88,47 @@ const Core = ({questions, appLocale, showDefaultResult, onComplete, customResult
     setButtons({});
 
     if (currentQuestionIndex + 1 === questions.length) {
-      setEndQuiz(true)
+      setEndQuiz(true);
     } else {
-      setCurrentQuestionIndex(currentQuestionIndex + 1)
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
     }
   };
 
   const handleChange = (event) => {
-    setFilteredValue(event.target.value)
+    setFilteredValue(event.target.value);
   };
 
   const renderAnswerInResult = (question, userInputIndex) => {
-    const {answers, correctAnswer, questionType} = question;
-    let {answerSelectionType} = question;
+    const { answers, correctAnswer, questionType } = question;
+    let { answerSelectionType } = question;
     let answerBtnCorrectClassName;
     let answerBtnIncorrectClassName;
 
     // Default single to avoid code breaking due to automatic version upgrade
     answerSelectionType = answerSelectionType || 'single';
 
-
     return answers.map((answer, index) => {
       if (answerSelectionType === 'single') {
-          // correctAnswer - is string
+        // correctAnswer - is string
         answerBtnCorrectClassName = (`${index + 1}` === correctAnswer ? 'correct' : '');
         answerBtnIncorrectClassName = (`${userInputIndex}` !== correctAnswer && `${index + 1}` === `${userInputIndex}` ? 'incorrect' : '');
       } else {
-          // correctAnswer - is array of numbers
+        // correctAnswer - is array of numbers
         answerBtnCorrectClassName = (correctAnswer.includes(index + 1) ? 'correct' : '');
-        answerBtnIncorrectClassName = (!correctAnswer.includes(index + 1) && userInputIndex.includes(index + 1) ? 'incorrect' : '')
+        answerBtnIncorrectClassName = (!correctAnswer.includes(index + 1) && userInputIndex.includes(index + 1) ? 'incorrect' : '');
       }
 
       return (
-          <div key={index}>
-            <button disabled={true}
-                    className={"answerBtn btn " + answerBtnCorrectClassName + answerBtnIncorrectClassName}>
-              {questionType === 'text' && <span>{answer}</span>}
-              {questionType === 'photo' && <img src={answer} alt="image"/>}
-            </button>
-          </div>
-      )
+        <div key={index}>
+          <button
+            disabled
+            className={`answerBtn btn ${answerBtnCorrectClassName}${answerBtnIncorrectClassName}`}
+          >
+            {questionType === 'text' && <span>{answer}</span>}
+            {questionType === 'photo' && <img src={answer} alt="image" />}
+          </button>
+        </div>
+      );
     });
   };
 
@@ -136,37 +139,37 @@ const Core = ({questions, appLocale, showDefaultResult, onComplete, customResult
     if (filteredValue !== 'all') {
       if (filteredValue === 'correct') {
         filteredQuestions = questions.filter((question, index) => correct.indexOf(index) !== -1);
-        filteredUserInput = userInput.filter((input, index) => correct.indexOf(index) !== -1)
+        filteredUserInput = userInput.filter((input, index) => correct.indexOf(index) !== -1);
       } else {
         filteredQuestions = questions.filter((question, index) => incorrect.indexOf(index) !== -1);
-        filteredUserInput = userInput.filter((input, index) => incorrect.indexOf(index) !== -1)
+        filteredUserInput = userInput.filter((input, index) => incorrect.indexOf(index) !== -1);
       }
     }
 
-    return (filteredQuestions ? filteredQuestions : questions).map((question, index) => {
+    return (filteredQuestions || questions).map((question, index) => {
       const userInputIndex = filteredUserInput ? filteredUserInput[index] : userInput[index];
 
       // Default single to avoid code breaking due to automatic version upgrade
-      let answerSelectionType = question.answerSelectionType || 'single';
+      const answerSelectionType = question.answerSelectionType || 'single';
 
       return (
-          <div className="result-answer-wrapper" key={index + 1}>
-            <h3 dangerouslySetInnerHTML={rawMarkup(`Q${question.questionIndex}: ${question.question}`)}/>
-            {question.questionPic && <img src={question.questionPic} alt="image"/>}
-            {renderTags(answerSelectionType, question.correctAnswer.length, question.segment)}
-            <div className="result-answer">
-              {renderAnswerInResult(question, userInputIndex)}
-            </div>
-            <Explanation question={question} isResultPage={true}/>
+        <div className="result-answer-wrapper" key={index + 1}>
+          <h3 dangerouslySetInnerHTML={rawMarkup(`Q${question.questionIndex}: ${question.question}`)} />
+          {question.questionPic && <img src={question.questionPic} alt="image" />}
+          {renderTags(answerSelectionType, question.correctAnswer.length, question.segment)}
+          <div className="result-answer">
+            {renderAnswerInResult(question, userInputIndex)}
           </div>
-      )
-    })
+          <Explanation question={question} isResultPage />
+        </div>
+      );
+    });
   }, [endQuiz, filteredValue]);
 
   const renderAnswers = (question, buttons) => {
-    const {answers, correctAnswer, questionType} = question;
-    let {answerSelectionType} = question;
-    const onClickAnswer = index => checkAnswer(index + 1, correctAnswer, answerSelectionType, {
+    const { answers, correctAnswer, questionType } = question;
+    let { answerSelectionType } = question;
+    const onClickAnswer = (index) => checkAnswer(index + 1, correctAnswer, answerSelectionType, {
       userInput,
       userAttempt,
       currentQuestionIndex,
@@ -181,119 +184,116 @@ const Core = ({questions, appLocale, showDefaultResult, onComplete, customResult
       setIncorrect,
       setShowNextQuestionButton,
       setUserInput,
-      setUserAttempt
+      setUserAttempt,
     });
 
     // Default single to avoid code breaking due to automatic version upgrade
     answerSelectionType = answerSelectionType || 'single';
 
-    return answers.map((answer, index) =>
+    return answers.map((answer, index) => (
       <Fragment key={index}>
-            {(buttons[index] !== undefined)
-                ? (<button
-                    disabled={buttons[index].disabled || false}
-                    className={`${buttons[index].className} answerBtn btn`}
-                    onClick={() => onClickAnswer(index)}
-                >
-                  {questionType === 'text' && <span>{answer}</span>}
-                  {questionType === 'photo' && <img src={answer} alt="image"/>}
-                </button>)
-                : <button
-                    onClick={() => onClickAnswer(index)}
-                    className="answerBtn btn"
-                >
-                  {questionType === 'text' && answer}
-                  {questionType === 'photo' && <img src={answer} alt="image"/>}
-                </button>
-            }
-          </Fragment>
-    )
+        {(buttons[index] !== undefined)
+          ? (
+            <button
+              disabled={buttons[index].disabled || false}
+              className={`${buttons[index].className} answerBtn btn`}
+              onClick={() => onClickAnswer(index)}
+            >
+              {questionType === 'text' && <span>{answer}</span>}
+              {questionType === 'photo' && <img src={answer} alt="image" />}
+            </button>
+          )
+          : (
+            <button
+              onClick={() => onClickAnswer(index)}
+              className="answerBtn btn"
+            >
+              {questionType === 'text' && answer}
+              {questionType === 'photo' && <img src={answer} alt="image" />}
+            </button>
+          )}
+      </Fragment>
+    ));
   };
 
   const renderTags = (answerSelectionType, numberOfSelection, segment) => {
     const {
       singleSelectionTagText,
       multipleSelectionTagText,
-      pickNumberOfSelection
+      pickNumberOfSelection,
     } = appLocale;
 
     return (
-        <div className="tag-container">
-          {answerSelectionType === 'single' &&
-          <span className="single selection-tag">{singleSelectionTagText}</span>
-          }
-          {answerSelectionType === 'multiple' &&
-          <span className="multiple selection-tag">{multipleSelectionTagText}</span>}
-          <span className="number-of-selection">
-            {pickNumberOfSelection.replace("<numberOfSelection>", numberOfSelection)}
-          </span>
-          {segment && <span className="selection-tag segment">{segment}</span>}
-        </div>
-    )
+      <div className="tag-container">
+        {answerSelectionType === 'single'
+          && <span className="single selection-tag">{singleSelectionTagText}</span>}
+        {answerSelectionType === 'multiple'
+          && <span className="multiple selection-tag">{multipleSelectionTagText}</span>}
+        <span className="number-of-selection">
+          {pickNumberOfSelection.replace('<numberOfSelection>', numberOfSelection)}
+        </span>
+        {segment && <span className="selection-tag segment">{segment}</span>}
+      </div>
+    );
   };
 
   const renderResult = () => (
-      <div className="card-body">
-        <h2>
-          {appLocale.resultPageHeaderText.replace("<correctIndexLength>", correct.length).replace("<questionLength>", questions.length)}
-        </h2>
-        <h2>
-          {appLocale.resultPagePoint.replace("<correctPoints>", correctPoints).replace("<totalPoints>", totalPoints)}
-        </h2>
-        <br/>
-        <QuizResultFilter
-            filteredValue={filteredValue}
-            handleChange={handleChange}
-            appLocale={appLocale}
-        />
-        {renderQuizResultQuestions()}
-      </div>
+    <div className="card-body">
+      <h2>
+        {appLocale.resultPageHeaderText.replace('<correctIndexLength>', correct.length).replace('<questionLength>', questions.length)}
+      </h2>
+      <h2>
+        {appLocale.resultPagePoint.replace('<correctPoints>', correctPoints).replace('<totalPoints>', totalPoints)}
+      </h2>
+      <br />
+      <QuizResultFilter
+        filteredValue={filteredValue}
+        handleChange={handleChange}
+        appLocale={appLocale}
+      />
+      {renderQuizResultQuestions()}
+    </div>
   );
 
   return (
-      <div className="questionWrapper">
-        {!endQuiz &&
+    <div className="questionWrapper">
+      {!endQuiz
+        && (
         <div className="questionWrapperBody">
           <div className="questionModal">
             <InstantFeedback
-                question={question}
-                showInstantFeedback={showInstantFeedback}
-                correctAnswer={correctAnswer}
-                incorrectAnswer={incorrectAnswer}
+              question={question}
+              showInstantFeedback={showInstantFeedback}
+              correctAnswer={correctAnswer}
+              incorrectAnswer={incorrectAnswer}
             />
           </div>
-          <div>{appLocale.question} {currentQuestionIndex + 1}:</div>
-          <h3 dangerouslySetInnerHTML={rawMarkup(question && question.question)}/>
-          {question && question.questionPic && <img src={question.questionPic} alt="image"/>}
+          <div>
+            {appLocale.question}
+            {' '}
+            {currentQuestionIndex + 1}
+            :
+          </div>
+          <h3 dangerouslySetInnerHTML={rawMarkup(question && question.question)} />
+          {question && question.questionPic && <img src={question.questionPic} alt="image" />}
           {question && renderTags(answerSelectionTypeState, question.correctAnswer.length, question.segment)}
           {question && renderAnswers(question, buttons)}
-          {showNextQuestionButton &&
+          {showNextQuestionButton
+          && (
           <div>
             <button onClick={() => nextQuestion(currentQuestionIndex)} className="nextQuestionBtn btn">
               {appLocale.nextQuestionBtn}
             </button>
           </div>
-          }
+          )}
         </div>
-        }
-        {endQuiz && showDefaultResultState && customResultPage === undefined &&
-          renderResult()
-        }
-        {endQuiz && !showDefaultResultState && customResultPage !== undefined &&
-          customResultPage(questionSummary)
-        }
-      </div>
+        )}
+      {endQuiz && showDefaultResultState && customResultPage === undefined
+          && renderResult()}
+      {endQuiz && !showDefaultResultState && customResultPage !== undefined
+          && customResultPage(questionSummary)}
+    </div>
   );
-};
-
-Core.propTypes = {
-  questions: PropTypes.array,
-  showDefaultResult: PropTypes.bool,
-  onComplete: PropTypes.func,
-  customResultPage: PropTypes.func,
-  showInstantFeedback: PropTypes.bool,
-  continueTillCorrect: PropTypes.bool,
-  appLocale: PropTypes.object
 };
 
 export default Core;
