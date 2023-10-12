e73a7c38fe94454d7156c6b978d4f068802053a2
shahriar hasan <shahriar.hasan.1523@gmail.com>
2023-10-12 16:29:19 +0600

    shuffle among answers feature added with valid correct answer

--- src/lib/Quiz.jsx
+++ src/lib/Quiz.jsx
@@ -1,22 +1,70 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import Core from './Core';
-import defaultLocale from './Locale';
-import './styles.css';
+import React, { useState, useEffect, useCallback } from "react";
+import Core from "./Core";
+import defaultLocale from "./Locale";
+import "./styles.css";
 
 const Quiz = function ({
-  quiz, shuffle, showDefaultResult, onComplete, customResultPage,
-  showInstantFeedback, continueTillCorrect, revealAnswerOnSubmit,
-  allowNavigation, onQuestionSubmit, disableSynopsis,
+  quiz,
+  shuffle,
+  showDefaultResult,
+  onComplete,
+  customResultPage,
+  showInstantFeedback,
+  continueTillCorrect,
+  revealAnswerOnSubmit,
+  allowNavigation,
+  onQuestionSubmit,
+  disableSynopsis,
 }) {
   const [start, setStart] = useState(false);
   const [questions, setQuestions] = useState(quiz.questions);
-  const nrOfQuestions = (quiz.nrOfQuestions && quiz.nrOfQuestions < quiz.questions.length) ? quiz.nrOfQuestions : quiz.questions.length;
-
+  const nrOfQuestions =
+    quiz.nrOfQuestions && quiz.nrOfQuestions < quiz.questions.length
+      ? quiz.nrOfQuestions
+      : quiz.questions.length;
+  const shuffleAnswerSequence = (oldQuestions = []) => {
+    const newQuestions = oldQuestions.map((question) => {
+      const answerWithIndex = question.answers?.map((ans, i) => [ans, i]);
+      const shuffledAnswersWithIndex = answerWithIndex.sort(
+        () => Math.random() - 0.5
+      );
+      const shuffledAnswers = shuffledAnswersWithIndex.map((ans) => ans[0]);
+      if (question.answerSelectionType === "single") {
+        const oldCorrectAnswer = question.correctAnswer;
+        const newCorrectAnswer =
+          shuffledAnswersWithIndex.findIndex(
+            (ans) => `${ans[1] + 1}` === `${oldCorrectAnswer}`
+          ) + 1;
+        return {
+          ...question,
+          correctAnswer: `${newCorrectAnswer}`,
+          answers: shuffledAnswers,
+        };
+      }
+      if (question.answerSelectionType === "multiple") {
+        const oldCorrectAnswer = question.correctAnswer;
+        const newCorrectAnswer = oldCorrectAnswer.map(
+          (cans) =>
+            shuffledAnswersWithIndex.findIndex(
+              (ans) => `${ans[1] + 1}` === `${cans}`
+            ) + 1
+        );
+        return {
+          ...question,
+          correctAnswer: newCorrectAnswer,
+          answers: shuffledAnswers,
+        };
+      }
+      return question;
+    });
+    return newQuestions;
+  };
   const shuffleQuestions = useCallback((q) => {
     for (let i = q.length - 1; i > 0; i -= 1) {
       const j = Math.floor(Math.random() * (i + 1));
       [q[i], q[j]] = [q[j], q[i]];
     }
+    q = shuffleAnswerSequence(q);
     q.length = nrOfQuestions;
     return q;
   }, []);
@@ -27,28 +75,35 @@ const Quiz = function ({
 
   useEffect(() => {
     if (shuffle) {
-      setQuestions(shuffleQuestions(quiz.questions));
+      setQuestions([...shuffleQuestions(quiz.questions)]);
     } else {
       quiz.questions.length = nrOfQuestions;
       setQuestions(quiz.questions);
     }
 
-    setQuestions(questions.map((question, index) => ({
-      ...question,
-      questionIndex: index + 1,
-    })));
+    // setQuestions(
+    //   questions.map((question, index) => ({
+    //     ...question,
+    //     questionIndex: index + 1,
+    //   }))
+    // );
   }, [start]);
 
   const validateQuiz = (q) => {
     if (!q) {
-      console.error('Quiz object is required.');
+      console.error("Quiz object is required.");
       return false;
     }
 
     for (let i = 0; i < questions.length; i += 1) {
       const {
-        question, questionType, answerSelectionType, answers, correctAnswer,
+        question,
+        questionType,
+        answerSelectionType,
+        answers,
+        correctAnswer,
       } = questions[i];
+      console.log({ q: questions[i] });
       if (!question) {
         console.error("Field 'question' is required.");
         return false;
@@ -58,8 +113,10 @@ const Quiz = function ({
         console.error("Field 'questionType' is required.");
         return false;
       }
-      if (questionType !== 'text' && questionType !== 'photo') {
-        console.error("The value of 'questionType' is either 'text' or 'photo'.");
+      if (questionType !== "text" && questionType !== "photo") {
+        console.error(
+          "The value of 'questionType' is either 'text' or 'photo'."
+        );
         return false;
       }
 
@@ -81,17 +138,26 @@ const Quiz = function ({
 
       if (!answerSelectionType) {
         // Default single to avoid code breaking due to automatic version upgrade
-        console.warn('Field answerSelectionType should be defined since v0.3.0. Use single by default.');
-        selectType = answerSelectionType || 'single';
+        console.warn(
+          "Field answerSelectionType should be defined since v0.3.0. Use single by default."
+        );
+        selectType = answerSelectionType || "single";
       }
 
-      if (selectType === 'single' && !(typeof selectType === 'string' || selectType instanceof String)) {
-        console.error('answerSelectionType is single but expecting String in the field correctAnswer');
+      if (
+        selectType === "single" &&
+        !(typeof selectType === "string" || selectType instanceof String)
+      ) {
+        console.error(
+          "answerSelectionType is single but expecting String in the field correctAnswer"
+        );
         return false;
       }
 
-      if (selectType === 'multiple' && !Array.isArray(correctAnswer)) {
-        console.error('answerSelectionType is multiple but expecting Array in the field correctAnswer');
+      if (selectType === "multiple" && !Array.isArray(correctAnswer)) {
+        console.error(
+          "answerSelectionType is multiple but expecting Array in the field correctAnswer"
+        );
         return false;
       }
     }
@@ -100,7 +166,7 @@ const Quiz = function ({
   };
 
   if (!validateQuiz(quiz)) {
-    return (null);
+    return null;
   }
 
   const appLocale = {
@@ -110,22 +176,25 @@ const Quiz = function ({
 
   return (
     <div className="react-quiz-container">
-      {!start
-          && (
+      {!start && (
+        <div>
+          <h2>{quiz.quizTitle}</h2>
           <div>
-            <h2>{quiz.quizTitle}</h2>
-            <div>{appLocale.landingHeaderText.replace('<questionLength>', nrOfQuestions)}</div>
-            {quiz.quizSynopsis
-              && (
-              <div className="quiz-synopsis">
-                {quiz.quizSynopsis}
-              </div>
-              )}
-            <div className="startQuizWrapper">
-              <button onClick={() => setStart(true)} className="startQuizBtn btn">{appLocale.startQuizBtn}</button>
-            </div>
+            {appLocale.landingHeaderText.replace(
+              "<questionLength>",
+              nrOfQuestions
+            )}
           </div>
+          {quiz.quizSynopsis && (
+            <div className="quiz-synopsis">{quiz.quizSynopsis}</div>
           )}
+          <div className="startQuizWrapper">
+            <button onClick={() => setStart(true)} className="startQuizBtn btn">
+              {appLocale.startQuizBtn}
+            </button>
+          </div>
+        </div>
+      )}
 
       {start && (
         <Core
