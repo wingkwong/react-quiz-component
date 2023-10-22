452f66465e688f3c2e3e5028b745f67709401f64
VishakhaSainani <vishakhasainani26@gmail.com>
2023-10-22 22:16:33 +0530

    Improved quiz filter design

--- src/lib/core-components/QuizResultFilter.jsx
+++ src/lib/core-components/QuizResultFilter.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const QuizResultFilter = function ({ filteredValue, handleChange, appLocale }) {
   return (
     <div className="quiz-result-filter">
-      <select value={filteredValue} onChange={handleChange}>
-        <option value="all">{appLocale.resultFilterAll}</option>
-        <option value="correct">{appLocale.resultFilterCorrect}</option>
+      <select value={filteredValue} onChange={handleChange} className='f'>
+        <option value="all" >{appLocale.resultFilterAll}</option>
+        <option value="correct" >{appLocale.resultFilterCorrect}</option>
         <option value="incorrect">{appLocale.resultFilterIncorrect}</option>
       </select>
     </div>
