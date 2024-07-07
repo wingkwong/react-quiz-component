33c3a3e015004535449d2f1f44c8031494483f3f
Jay <44129484+Jaykovsky@users.noreply.github.com>
2024-07-07 10:26:46 -0500

    feat: progress bar (#282)
    * --added fixed progress bar
    
    * --added props for readme and validation for progress bar input color
    
    * --progressBarColor in quiz object
    
    * --added some changes
    
    * --minor error fixed
    
    ---------
    
    Co-authored-by: աӄա <wingkwong.code@gmail.com>

--- src/docs/quiz.jsx
+++ src/docs/quiz.jsx
@@ -8,6 +8,7 @@ const quiz = {
   quizTitle: 'React Quiz Component Demo',
   quizSynopsis: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim',
   nrOfQuestions: '6',
+  progressBarColor: '#9de1f6',
   questions: [
     {
       question: 'How can you access the state of a component from inside of a member function?',
